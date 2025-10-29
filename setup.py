0004ae437c0d484df5c975481aaff9c43512e306
Daniela Wiepert <daniela.wiepert7@gmail.com>
2025-10-28 19:32:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ setup(
     install_requires=[
       "huggingface-hub==0.35.3",
       "librosa==0.11.0",
-      "numpy",
-      "openpyxl",
-      "pandas",
-      "scikit-learn",
-      "scipy",
-      "torch",
-      "torchaudio",
-      "torchvision",
-      "tqdm",
-      "transformers"
+      "numpy==2.2.6",
+      "openpyxl==3.1.5",
+      "pandas==2.3.3",
+      "scikit-learn==1.7.2",
+      "scipy==1.15.3",
+      "torch==2.8.0",
+      "torchaudio==2.8.0",
+      "torchvision==0.23.0",
+      "tqdm==4.67.1",
+      "transformers==4.57.0"
     ],
     include_package_data=False,
     version = __version__
